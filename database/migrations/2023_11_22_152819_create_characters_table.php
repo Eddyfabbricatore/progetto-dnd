1d817c1514aaffa4e1e33d4cc1ee5a357a20d722
Pirisinu <palazzialessandro2@gmail.com>
2023-11-22 16:50:20 +0100

    [ add ] table_characters

--- database/migrations/2023_11_22_152819_create_characters_table.php
+++ database/migrations/2023_11_22_152819_create_characters_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 50);
+            $table->string('height', 10);
+            $table->string('weight', 10);
+            $table->text('background');
+            $table->string('image');
+            $table->tinyInteger('armor_class')->unsigned();
+            $table->tinyInteger('str')->unsigned();
+            $table->tinyInteger('dex')->unsigned();
+            $table->tinyInteger('con')->unsigned();
+            $table->tinyInteger('int')->unsigned();
+            $table->tinyInteger('wis')->unsigned();
+            $table->tinyInteger('cha')->unsigned();
+
             $table->timestamps();
         });
     }
